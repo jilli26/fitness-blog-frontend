c78407ded523673ef1ff174aa20fb26a73808326
github username <github email address>
2018-02-22 14:00:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header >
+          <h1 >Jil's fitnes blog</h1>
+        </header>
+        <p className="App-intro">
+          Blah blah blah
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
