b8f31010c423ec423fe3bcfb40afd2a64a605967
github username <github email address>
2018-02-23 14:17:07 -0500

    added redux, thunk, router, devtools and set up redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
+//import all the reducers and use combinereducers
+import searchesReducer from './reducers/searchesReducer'
+import studiosReducer from './reducers/studiosReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootReducer = combineReducers({ studiosReducer, searchesReducer })
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
